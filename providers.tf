259afd78f5c0e7eba743462bb33376778fa05c27
Javier Caparo <55814804+javier-caparo@users.noreply.github.com>
2024-09-13 21:00:26 -0500

    Create providers.tf

---
+++ providers.tf
@@ -0,0 +1,31 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "3.83.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "3.6.0"
+    }
+    null = {
+      source  = "hashicorp/null"
+      version = "3.2.2"
+    }
+
+    time = {
+      source = "hashicorp/time"
+    }
+  }
+
+  backend "azurerm" {
+  }
+}
+
+provider "azurerm" {
+  features {
+  }
+
+  skip_provider_registration = true
+  subscription_id            = var.SUBSCRIPTION_ID
+}
